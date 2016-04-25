282944972fafc87947c4b5d974492c1d5b32b4c5
Tim Lovorn <tflovorn@austin.utexas.edu>
2016-04-25 12:01:36 -0500

    break up plot setup in plot_ds

--- tmd/bilayer/plot_ds.py
+++ tmd/bilayer/plot_ds.py
@@ -16,70 +16,63 @@ def ds_from_prefixes(prefixes):
 
     return ds
 
-def wrap_cell(ds, prefixes):
-    wrapped_ds, wrapped_prefixes = [], []
-    for d, prefix in zip(ds, prefixes):
+def wrap_cell(ds, values):
+    wrapped_ds, wrapped_values = [], []
+    for d, v in zip(ds, values):
         da, db = d[0], d[1]
         wrapped_ds.append(d)
-        wrapped_prefixes.append(prefix)
+        wrapped_values.append(v)
 
         if da == 0.0 and db == 0.0:
             wrapped_ds.append((1.0, 1.0))
-            wrapped_prefixes.append(prefix)
+            wrapped_values.append(v)
             wrapped_ds.append((0.0, 1.0))
-            wrapped_prefixes.append(prefix)
+            wrapped_values.append(v)
             wrapped_ds.append((1.0, 0.0))
-            wrapped_prefixes.append(prefix)
+            wrapped_values.append(v)
         elif da == 0.0:
             wrapped_ds.append((1.0, db))
-            wrapped_prefixes.append(prefix)
+            wrapped_values.append(v)
         elif db == 0.0:
             wrapped_ds.append((da, 1.0))
-            wrapped_prefixes.append(prefix)
+            wrapped_values.append(v)
 
-    return wrapped_ds, wrapped_prefixes
+    return wrapped_ds, wrapped_values
 
-def _main():
-    gconf = global_config()
-    work = gconf["work_base"]
-    
-    global_prefix = "MoS2_WS2"
-    prefixes = get_prefixes(work, global_prefix)
-    ds = ds_from_prefixes(prefixes)
+def sorted_d_group(ds, values):
+    dvs = list(zip(ds, values))
+    dvs = sorted(dvs, key=lambda dp: dp[0][1])
+    dvs = sorted(dvs, key=lambda dp: dp[0][0])
+    return dvs
 
-    ds, prefixes = wrap_cell(ds, prefixes)
-    print(ds)
-    print(prefixes)
-
-    dps = list(zip(ds, prefixes))
-    dps = sorted(dps, key=lambda dp: dp[0][1])
-    dps = sorted(dps, key=lambda dp: dp[0][0])
-
-    base = None
-    for d_i, (d, prefix) in enumerate(dps):
-        if d == (0.0, 0.0):
-            base = d_i
-
-    if base is None:
-        raise ValueError("d = (0, 0) not found")
-
+def get_energies(work, dps):
     energies = []
     for d, prefix in dps:
-        print(d, prefix)
         wannier_dir = os.path.join(work, prefix, "wannier")
         scf_path = os.path.join(wannier_dir, "scf.out")
         energy = total_energy_eV_from_scf(scf_path)
         energies.append(energy)
 
-    dps, energies = wrap_cell(dps, energies)
+    return energies
 
-    base_energy = energies[base]
+def energies_relative_to(energies, dps, base_d):
+    base_d_index = None
+    for d_i, (d, prefix) in enumerate(dps):
+        if d == (0.0, 0.0):
+            base_d_index = d_i
+
+    if base_d_index is None:
+        raise ValueError("d = (0, 0) not found")
+
+    base_energy = energies[base_d_index]
     energies_rel_meV = []
     for E in energies:
         E_rel = (E - base_energy) * 1000
         energies_rel_meV.append(E_rel)
-        print(E_rel)
 
+    return energies_rel_meV
+
+def plot_d_vals(plot_name, title, dps, values):
     xs, ys = [], []
     xs_set, ys_set = set(), set()
     for d, prefix in dps:
@@ -91,9 +84,7 @@ def _main():
     num_xs = len(xs_set)
     num_ys = len(ys_set)
 
-    X = np.array(xs).reshape((num_xs, num_ys))
-    Y = np.array(ys).reshape((num_xs, num_ys))
-    C_E = np.array(energies_rel_meV).reshape((num_xs, num_ys))
+    C_E = np.array(values).reshape((num_xs, num_ys))
 
     plt.xlabel("$d_a$")
     plt.ylabel("$d_b$")
@@ -109,14 +100,28 @@ def _main():
     plt.xticks(np.linspace(0.0, num_xs-1, num_ticks_xs, endpoint=True), d_ticks_xs)
     plt.yticks(np.linspace(0.0, num_ys-1, num_ticks_ys, endpoint=True), d_ticks_ys)
 
-    #plt.pcolormesh(X, Y, C_E.T, cmap=cm.viridis)
-    #plt.pcolormesh(xs, ys, C_E.T, cmap=cm.viridis)
     plt.imshow(C_E.T, origin='lower', interpolation='none', cmap=cm.viridis)
     plt.colorbar()
-    plt.title("$\\Delta E$ [meV]")
-    #plt.pcolor(C_E.T, cmap=cm.viridis)
-    #plt.scatter(xs, ys, c=energies_rel_meV)
-    plt.savefig("{}.png".format(global_prefix), bbox_inches='tight', dpi=500)
+    plt.title(title)
+    plt.savefig("{}.png".format(plot_name), bbox_inches='tight', dpi=500)
+
+def _main():
+    gconf = global_config()
+    work = os.path.expandvars(gconf["work_base"])
+    
+    global_prefix = "MoSe2_WSe2"
+    prefixes = get_prefixes(work, global_prefix)
+    ds = ds_from_prefixes(prefixes)
+
+    ds, prefixes = wrap_cell(ds, prefixes)
+    dps = sorted_d_group(ds, prefixes)
+
+    energies = get_energies(work, dps)
+    energies_rel_meV = energies_relative_to(energies, dps, (0.0, 0.0))
+
+    E_title = "$\\Delta E$ [meV]"
+    E_plot_name = "{}_energies".format(global_prefix)
+    plot_d_vals(E_plot_name, E_title, dps, energies_rel_meV)
 
 if __name__ == "__main__":
     _main()
